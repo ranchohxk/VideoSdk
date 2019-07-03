4bef0ab29c255f83c7ba061eb70c90e535255eda
hxk <15088133707@163.com>
2019-07-03 14:22:34 +0800

    update

--- VideoSDK/video_filter/.externalNativeBuild/cmake/debug/armeabi-v7a/cmake_install.cmake
+++ VideoSDK/video_filter/.externalNativeBuild/cmake/debug/armeabi-v7a/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/VideoSDK/video_filter
+# Install script for directory: E:/GitHub/VideoSdk/VideoSDK/video_filter
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/VideoSDK/video_filter/.externalNativeBuild/cmake/debug/armeabi-v7a/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "E:/GitHub/VideoSdk/VideoSDK/video_filter/.externalNativeBuild/cmake/debug/armeabi-v7a/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
